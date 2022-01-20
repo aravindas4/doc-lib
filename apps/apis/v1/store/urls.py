83ce4335d8f2a8e5e46d585357bf30d53d9b3564
Aravinda <ash.g.education@gmail.com>
2022-01-20 13:40:42 +0530

    Add test utils

--- apps/apis/v1/store/urls.py
+++ apps/apis/v1/store/urls.py
@@ -1,1 +1,15 @@
-urlpatterns = []
+from django.urls import include, path
+
+from rest_framework.routers import SimpleRouter
+
+from .views import DocumentViewSet
+
+app_name = "store-v1"
+
+router = SimpleRouter()
+
+router.register(r"document", DocumentViewSet, basename="document")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
