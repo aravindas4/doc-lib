944bc293e26c2a4ce2cf4f064ef25e13e54d15b8
Aravinda <ash.g.education@gmail.com>
2022-01-20 07:59:11 +0530

    Add migrations

--- apps/store/models.py
+++ apps/store/models.py
@@ -11,7 +11,7 @@ def get_short_uuid():
 
 class BaseModel(models.Model):
     """
-    Abstract model that acts as parent all the concreate models in the project.
+    Abstract model that acts as parent all the concrete models in the project.
     """
 
     # Makes sure that an pk is tamper-proof and unpredictable
@@ -20,8 +20,6 @@ class BaseModel(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.CharField(max_length=250, null=True, blank=True)
-    modified_by = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta:
         abstract = True
@@ -36,3 +34,58 @@ class User(AbstractUser, BaseModel):
 
     def __str__(self):
         return f"{self.get_username()}, {self.email} ({self.id})"
+
+
+class Document(BaseModel):
+    """The model that represents the uploaded document."""
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="documents"
+    )
+    # Local file storage reference
+    file = models.FileField(blank=True, upload_to="documents")
+
+    # Shared users
+    shared_users = models.ManyToManyField(
+        User,
+        through="UserDocument",  # Explicit because to add future changes
+        related_name="shared_documents",
+    )
+
+    class Meta:
+        verbose_name_plural = "Documents"
+        ordering = ("-created_at",)  # Latest first
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        if self.pk is None:  # Creation time
+            pass
+
+        super().save(
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+        )
+
+
+class UserDocument(BaseModel):
+    """
+    Intermediate model that stores relationship between users and shared
+    documents.
+    """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_documents"
+    )
+    document = models.ForeignKey(
+        Document, on_delete=models.CASCADE, related_name="user_documents"
+    )
+
+    class Meta:
+        verbose_name_plural = "User Documents"
