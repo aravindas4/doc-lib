146ff7286930f84feccc0089e74f25c45ea78a60
Aravinda S Holla <30445925+aravindas4@users.noreply.github.com>
2022-01-21 15:51:33 +0530

    Add delete APIs (#12)
    - Add delete APIs

--- apps/apis/v1/store/tests.py
+++ apps/apis/v1/store/tests.py
@@ -287,4 +287,38 @@ class DocumentAPITest(APITest):
         pass
 
     def test_delete(self):
-        pass
+        # Case 1: Non shared User
+        # Arrange
+        user1 = UserFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=self.get_auth_header(user1))
+        expected_response = {"detail": "Not found."}
+
+        # Act
+        response = self.client.delete(self.default_url)
+
+        # Assert
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), expected_response)
+
+        # Case 2: Shared User
+        # Arrange
+        UserDocumentFactory(user=user1, document=self.default)
+
+        # Act
+        response = self.client.delete(self.default_url)
+
+        # Assert
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), expected_response)
+
+        # Case 3: Owner
+        # Arrange
+        self.client.credentials(
+            HTTP_AUTHORIZATION=self.get_auth_header(self.default.owner)
+        )
+
+        # Act
+        response = self.client.delete(self.default_url)
+
+        # Assert
+        self.assertEqual(response.status_code, 204)
