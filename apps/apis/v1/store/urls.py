f3536f4a8600104f7f74a2a27687646f2db69117
Aravinda <ash.g.education@gmail.com>
2022-01-21 16:32:41 +0530

    Add user readonly apis

--- apps/apis/v1/store/urls.py
+++ apps/apis/v1/store/urls.py
@@ -2,13 +2,14 @@ from django.urls import include, path
 
 from rest_framework.routers import SimpleRouter
 
-from .views import DocumentViewSet
+from .views import DocumentViewSet, UserViewSet
 
 app_name = "store-v1"
 
 router = SimpleRouter()
 
 router.register(r"document", DocumentViewSet, basename="document")
+router.register(r"user", UserViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
